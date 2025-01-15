26e6ac281d30020bc3e871750b4af715d507a39b
feiyoungi <146308282+feiyoungi@users.noreply.github.com>
2025-01-15 02:56:19 -0500

    Update

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+
+function navigateToPage(url) {
+    // Fade out all images
+    const images = document.querySelectorAll('.image-wrapper img');
+    images.forEach(img => {
+        img.style.opacity = '0';
+    });
+
+    // Navigate to the new page after the fade-out animation
+    setTimeout(() => {
+        window.location.href = url;
+    }, 500); // Match the transition duration
+}
+
